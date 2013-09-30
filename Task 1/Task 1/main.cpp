a2922aba917cd0b276f5e3b8884ec37ecceae0b2
Crazy-G14 <graeme.black007@hotmail.co.uk>
2013-09-30 14:48:17 +0100

    Init commit (Broken)
    Task two is not complete, may not compile.

---
+++ Task 1/Task 1/main.cpp
@@ -0,0 +1,98 @@
+// Include necessary build libraries
+#pragma comment(lib, "glfw3dll")
+#pragma comment(lib, "OpenGL32")
+
+// States that we are using the DLL for GLFW
+#define GLFW_DLL
+
+// Include files
+#include <GLFW\glfw3.h>
+#include <chrono>
+
+using namespace std::chrono;
+
+// Window opened via GLFW
+GLFWwindow* window;
+
+// Flag used to monitor state of application
+bool running = true;
+
+// Initialises the application
+bool initialise()
+{
+	// Set clear colour to cyan
+	glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+	return true;
+}
+
+// Updates the application
+void update(double deltaTime)
+{
+	// Check if escape pressed or window is closed
+	running = !glfwGetKey(window, GLFW_KEY_ESCAPE) && !glfwWindowShouldClose(window);
+}
+
+// Renders the application
+void render()
+{
+	// Clear the scene
+	glClear(GL_COLOR_BUFFER_BIT);
+	// sWAP FRONT AND BACK BUFFERS
+	glfwSwapBuffers(window);
+}
+
+// Main Operation
+int main()
+{
+	// Try and initialise GLFW
+	if (!glfwInit())
+		return -1;
+
+	// Create a window for OpenGL rendering
+	window = (glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr));
+	// Check if window was created
+	if (window == nullptr)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	// Make the window's context current
+	glfwMakeContextCurrent(window);
+
+	// Initialise the application
+	if (!initialise())
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	// Monitor the elapsed time per frame
+	auto currentTimeStamp = system_clock::now();
+	auto prevTimeStamp = system_clock::now();
+
+	// Loop until running set to false
+	while (running)
+	{
+		// Get current time
+		currentTimeStamp = system_clock::now();
+		// Calculate elapsed time
+		auto elapsed = duration_cast<milliseconds>(currentTimeStamp - prevTimeStamp);
+		// Convert to fractions of a second
+		auto seconds = double(elapsed.count()) / 1000.0;
+
+		// Update application
+		update (seconds);
+		// Render scene
+		render();
+		//Poll for and process events
+		glfwPollEvents();
+		// Set previous time stamp to current time stamp
+		prevTimeStamp = currentTimeStamp;
+	}
+
+	// Terminate GLFW
+	glfwTerminate();
+
+	return -1;
+}
